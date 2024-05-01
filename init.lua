1c4d5ced8001ac06f11e35c9127998368d0b1f4a
Nicole <nd3644@gmail.com>
2024-05-01 19:19:52 +0100

    first change! New tab sizes

--- init.lua
+++ init.lua
@@ -42,20 +42,6 @@ What is Kickstart?
     - (or HTML version): https://neovim.io/doc/user/lua-guide.html
 
 Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
   Next, run AND READ `:help`.
     This will open up a help window with some basic information
     about reading, navigating and searching the builtin help documentation.
@@ -212,6 +198,11 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
+
+
+-- nd: Set tab sizes
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 -- [[ Configure and install plugins ]]
 --
